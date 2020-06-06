5e26fbfd4e76282ee9b7b6db01f2b6b6abca5044
mario-cp <39974830+mario-cp@users.noreply.github.com>
2020-06-05 22:36:26 -0500

    Margenes
    Margen superior e inferior

--- tabulador/p2c.ps1
+++ tabulador/p2c.ps1
@@ -1,9 +1,9 @@
-.\pdftotext.exe -nopgbrk -simple .\Tab.pdf t.txt
+.\pdftotext.exe -l 242 -margint 150 -marginb 70 -nopgbrk -simple .\202004.pdf 202004.txt
 $segundas="^\s{2,}(.*)$"
 $rxclave="^(?<clave>[A-Z0-9]+)\b";
 $final=$regex="\b(?<unidad>(cala adi|diseño|fosa|Ha|hora|juego|junta|litro|lote|lto|muestra|p.t|pieza|prueba|pza-km|semana|serie|toma|visita|an\wlisis|estudi\w{1,2}|kg(/uso)?|(m([23])?|pza|ton)([/-](d\wa|est(ac)?|km))?))(\W|\b)\s+(?<precio>(\d{1,3}([,\.]\d{3})*|(\d+))(\.\d{2})?)$";
 $espacios="\s+";
-$rawtext=Get-Content -Path ./t.txt
+$rawtext=Get-Content -Path ./202004.txt
 del .\catalogo.csv;
 $clave="";
 $unidad="";
